220dfad0bed36f09c8034faf9524973f166b4d07
asifglobo786 <asif.kazmi@helloglobo.com>
2024-05-17 18:24:05 +0530

    first commit

--- DemoAppUITests/DemoAppUITests.swift
+++ DemoAppUITests/DemoAppUITests.swift
@@ -27,15 +27,7 @@ final class DemoAppUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
